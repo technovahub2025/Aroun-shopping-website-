081b11e5df6101a7cd11c985395fa2066c23f9e0
Technovahub Career <technovahubcareer@gmail.com>
2025-10-23 20:19:52 +0530

    commmit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// Enable CORS for all origins
+app.use(cors({
+  origin: true,         
+  credentials: true     
+}));
 
 // Connect DB
 mongoose.connect(process.env.MONGO_URI)
@@ -25,7 +29,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
 
+
+
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
